5855eff6ad5ae3c47f90b1fbb8934f6c616bd570
eaortiz <eao7r@virginia.edu>
2021-08-17 15:02:38 -0400

    created pytime.py

--- pytime.py
+++ pytime.py
@@ -1,29 +1,34 @@
 import time
 
 localtime = time.asctime( time.localtime(time.time()) )
-print("Exercise 1 local current time :", localtime)
+print("Local current time :", localtime)
 
 tick = time.time()
 time.sleep(1)
 tock = time.time()
 
-print("Exercise 2 program took ", str(tock - tick), " seconds to run.")
+print("This program took ", str(tock - tick), " seconds to run.")
 
-print('Enter your name:')
-x = input()
-print('Exercise 3 Output: Hello, ' + x)
+
+w = True
+while w is True:
+    print('Enter your name:')
+    x = input()
+    if x.isalpha():
+        w = False
+        print('Hello, ' + x)
 
 print('Enter a number:')
 x = input()
 try:
     y = float(x)
-    print('Exercise 4 Output:  yes, ', x, " is a number")
+    print('Yes, ', x, " is a number")
 except:
-    print('Exercise 4 Output:  ', x, " is not a number")
+    print(x, " is not a number")
 
 print('Enter a list of things, each separated by a space:')
 x  = input()
 y = x.split()
-print("Exercise 5 Output: this is the list you entered:")
+print("This is the list you entered:")
 for n in y:
     print(n)
